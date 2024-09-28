ba43e7e63d6e6d3901b1b3a25c9f12506844839f
Yuliya <juravleva.yuliya202@gmail.com>
2024-09-28 23:13:01 +0300

    + дизайн

--- PZ_3/Практическое задание №3.py
+++ PZ_3/Практическое задание №3.py
@@ -7,7 +7,7 @@ import psutil
 
 root = Tk()
 root.title("Практические задачи")
-root.geometry("500x500")
+root.geometry("500x600+530+100")
 
 tasks = [" - выберите задачу - ", "Задание №1", "Задание №2", "Задание №3", "Задание №4", "Задание №5"]
 tasks_var = StringVar(value=tasks[0])
@@ -20,6 +20,7 @@ frame.pack(anchor=NW, padx=5, pady=1)
 
 label = ttk.Label(frame)
 label.pack()
+
 
 def z1():
     output = ""
@@ -41,15 +42,16 @@ def z1():
 
     label.config(text=output)
 
+
 def z2():
     import os
-    os.chdir(r"E:\Программы\Проекты Питон\PZ_3")
+    os.chdir(r"C:\Users\Юлия\Downloads\PZ_Python\PZ_3")
     # try:
     #     os.path.exists(r"E:\Программы\Проекты Питон\PZ_3\first.txt")
     # except False:
     #     print("21212")
 
-# =============================== создание файла ================================ #
+    # =============================== создание файла ================================ #
 
     file_name_var = tk.StringVar()
 
@@ -57,26 +59,26 @@ def z2():
     create_frame.pack(pady=10)
 
     def create_file():
-        file_name = file_name_var.get()+".txt"
+        file_name = file_name_var.get() + ".txt"
         if file_name:
             with open(file_name, 'w') as f:
                 f.write("")
             messagebox.showinfo("Успех", f"Файл '{file_name}' создан.")
-            #update_file_list() нужен дальше для динамики дирктория
         else:
             messagebox.showwarning("Ошибка", "Введите название файла.")
 
-    ttk.Label(create_frame, text="Название файла:").pack(side=tk.LEFT)
+    ttk.Label(create_frame, text="Название файла:").pack(side=tk.LEFT, ipadx=1)
     ttk.Entry(create_frame, textvariable=file_name_var).pack(side=tk.LEFT)
-    ttk.Button(create_frame, text="Создать файл", command=create_file).pack(side=tk.LEFT)
+    ttk.Button(create_frame, text="Создать файл", command=create_file).pack(side=tk.LEFT, padx=5)
 
-# ============================ отображение содержимого ============================ #
+    # ============================ отображение содержимого ============================ #
 
     content_frame = ttk.Frame()
     content_frame.pack(pady=10)
 
     def display_content():
-        file_name = file_name_var.get()+".txt"
+        file_name = file_name_var.get() + ".txt"
+
         if os.path.exists(file_name):
             with open(file_name, 'r', encoding="utf-8") as f:
                 content = f.read()
@@ -87,20 +89,21 @@ def z2():
         else:
             messagebox.showwarning("Ошибка", "Файл не существует.")
 
-    content_area = scrolledtext.ScrolledText(content_frame, width=30, height=10, state=tk.DISABLED)  # состояние изначально отключено
-    content_area.pack(side=tk.LEFT)
+    tk.Label(content_frame, text="Содержимое документа:").pack(pady=3)
 
-    tk.Button(content_frame, text="Отобразить содержимое", command=display_content).pack(side=tk.LEFT)
+    content_area = scrolledtext.ScrolledText(content_frame, width=30, height=10, state=tk.DISABLED)  # состояние
+    # изначально отключено
+    content_area.pack()
 
-# ============================ добавление текста ============================ #
+    tk.Button(content_frame, text="Отобразить содержимое файла", command=display_content).pack(pady=5)
+
+    # ============================ добавление текста ============================ #
 
     add_line_frame = tk.Frame()
     add_line_frame.pack(pady=10)
 
-    line_var = tk.StringVar()
-
     def add_line():
-        file_name = file_name_var.get()+".txt"
+        file_name = file_name_var.get() + ".txt"
         line_to_add = text_area.get("1.0", tk.END).strip()  # получаем весь текст из text_area
         if os.path.exists(file_name) and line_to_add:
             with open(file_name, 'a', encoding="utf-8") as f:
@@ -110,26 +113,30 @@ def z2():
         else:
             messagebox.showwarning("Ошибка", "Файл не существует или строка пуста.")
 
-    tk.Label(add_line_frame, text="Строка для добавления:").pack()
+    tk.Label(add_line_frame, text="Строка для добавления:").pack(pady=3)
 
     # Создаем многострочное текстовое поле
     text_area = scrolledtext.ScrolledText(add_line_frame, wrap=tk.WORD, width=30, height=5)
     text_area.pack()
 
-    tk.Button(add_line_frame, text="Добавить в файл", command=add_line).pack(side=tk.LEFT)
+    tk.Button(add_line_frame, text="Добавить в файл", command=add_line).pack(pady=5)
 
-# ============================ удаление текста ============================ #
+    # ============================ удаление текста ============================ #
+
+    delete_frame = tk.Frame()
+    delete_frame.pack(pady=10)
 
     def delete_file():
-        file_name = file_name_var.get()+".txt"
+        file_name = file_name_var.get() + ".txt"
         if os.path.exists(file_name):
             os.remove(file_name)
             messagebox.showinfo("Успех", f"Файл '{file_name}' удален.")
-            #update_file_list()
         else:
             messagebox.showwarning("Ошибка", "Файл не существует.")
 
-    tk.Button(text="Удалить файл", command=delete_file).pack(pady=10)
+    tk.Label(delete_frame, text="Для удаления файла нажмите на кнопку -> ").pack(pady=5, side=tk.LEFT)
+    tk.Button(delete_frame, text="Удалить файл", command=delete_file).pack(pady=10, side=tk.LEFT)
+
 
 def on_combobox_change(event):
     selected_task = tasks_var.get()
@@ -141,7 +148,8 @@ def on_combobox_change(event):
         z1()
     elif selected_task == "Задание №2":
         frame.pack_forget()
-        z2() #додумать дизайн
+        z2()
+
 
 combobox.bind("<<ComboboxSelected>>", on_combobox_change)
 on_combobox_change(None)
