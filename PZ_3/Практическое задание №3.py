f7b7e12b555227090450895e96bc65e211cea9ac
Yuliya <juravleva.yuliya202@gmail.com>
2024-09-29 21:52:28 +0300

    реворк фулл конструкции для корректного отображения

--- PZ_3/Практическое задание №3.py
+++ PZ_3/Практическое задание №3.py
@@ -4,25 +4,38 @@ from tkinter import messagebox, scrolledtext
 import tkinter as tk
 
 import psutil
+import os
 
-root = Tk()
-root.title("Практические задачи")
-root.geometry("500x600+530+100")
+web = Tk()
+web.title("Практические задачи")
+web.geometry("500x600+530+100")
 
-tasks = [" - выберите задачу - ", "Задание №1", "Задание №2", "Задание №3", "Задание №4", "Задание №5"]
+ttk.Label(text="Выбранная задача: ").pack(anchor=NW, padx=4, pady=1)
+
+tasks = [" - выберите задачу - ", "Задание №1", "Задание №2"]
 tasks_var = StringVar(value=tasks[0])
 
 combobox = ttk.Combobox(textvariable=tasks_var, values=tasks, state="readonly")
 combobox.pack(anchor=NW, padx=6, pady=6)
 
-frame = ttk.Frame(borderwidth=1, relief=SOLID, padding=[5, 5])
-frame.pack(anchor=NW, padx=5, pady=1)
+# фрейм для задач
+task_frame = Frame(web)
+task_frame.pack(anchor=NW, fill=BOTH)
 
-label = ttk.Label(frame)
-label.pack()
-
+def clear_task_frame():
+    # удаление всех деталей из фрейма с задачами
+    for widget in task_frame.winfo_children():
+        widget.destroy()
 
 def z1():
+    clear_task_frame()
+
+    frame1 = ttk.Frame(task_frame, borderwidth=3, relief=RIDGE, padding=[5, 5])
+    frame1.pack(anchor=NW, padx=5, pady=1)
+
+    label_z1 = ttk.Label(frame1)
+    label_z1.pack()
+
     output = ""
 
     parts = psutil.disk_partitions()
@@ -40,18 +53,18 @@ def z1():
 
         output += "+-------------------------------------+"
 
-    label.config(text=output)
-
+    label_z1.config(text=output)
 
 def z2():
-    import os
+    clear_task_frame()
+
     os.chdir(r"C:\Users\Юлия\Downloads\PZ_Python\PZ_3")
 
     # =============================== создание файла ================================ #
 
     file_name_var = tk.StringVar()
 
-    create_frame = ttk.Frame()
+    create_frame = ttk.Frame(task_frame)
     create_frame.pack(pady=10)
 
     def create_file():
@@ -61,7 +74,7 @@ def z2():
             return
         try:
             if os.path.exists(file_name):
-                raise FileExistsError() #raise для принудительного вызова ошибки
+                raise FileExistsError()  # raise для принудительного вызова ошибки
 
             with open(file_name, 'w') as f:
                 f.write("")
@@ -78,7 +91,7 @@ def z2():
 
     # ============================ отображение содержимого ============================ #
 
-    content_frame = ttk.Frame()
+    content_frame = ttk.Frame(task_frame)
     content_frame.pack(pady=10)
 
     def display_content():
@@ -106,7 +119,7 @@ def z2():
 
     # ============================ добавление текста ============================ #
 
-    add_line_frame = tk.Frame()
+    add_line_frame = tk.Frame(task_frame)
     add_line_frame.pack(pady=10)
 
     def add_line():
@@ -139,7 +152,7 @@ def z2():
 
     # ============================ удаление текста ============================ #
 
-    delete_frame = tk.Frame()
+    delete_frame = tk.Frame(task_frame)
     delete_frame.pack(pady=10)
 
     def delete_file():
@@ -157,20 +170,16 @@ def z2():
     tk.Label(delete_frame, text="Для удаления файла нажмите на кнопку -> ").pack(pady=5, side=tk.LEFT)
     tk.Button(delete_frame, text="Удалить файл", command=delete_file).pack(pady=10, side=tk.LEFT)
 
-
 def on_combobox_change(event):
     selected_task = tasks_var.get()
-    label.config(text="")
-    if selected_task == " - выберите задачу - ":
-        frame.pack_forget()
-    elif selected_task == "Задание №1":
-        frame.pack(anchor=NW, padx=5, pady=1)
+    if selected_task == "Задание №1":
         z1()
     elif selected_task == "Задание №2":
-        frame.pack_forget()
         z2()
+    else:
+        clear_task_frame()  # чистка фреймов, если выбрано " - выберите задачу - "
 
 combobox.bind("<<ComboboxSelected>>", on_combobox_change)
 on_combobox_change(None)
 
-root.mainloop()
+web.mainloop()
